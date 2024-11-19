8e7e831899a874cc091f4e99ff321dec6de16bcf
mlapusta <lapusta.mariia@gmail.com>
2024-11-19 15:41:02 +0100

    Lab3: simple file opening realization

--- src/main/java/com/example/texteditorproject/service/FileService.java
+++ src/main/java/com/example/texteditorproject/service/FileService.java
@@ -1,8 +1,21 @@
 package com.example.texteditorproject.service;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class FileService implements TextEditorService{
     @Override
     public void performOperation() {
 
+    }
+    public String readFile(File file) {
+        try {
+            return Files.readString(Path.of(file.toURI()));
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Error: Unable to read file.";
+        }
     }
 }
--- src/main/java/com/example/texteditorproject/ui/MainController.java
+++ src/main/java/com/example/texteditorproject/ui/MainController.java
@@ -1,4 +1,30 @@
 package com.example.texteditorproject.ui;
 
+import com.example.texteditorproject.service.FileService;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
 public class MainController {
+    @FXML
+    public TextArea textArea;
+
+    private final FileService fileService = new FileService();
+
+    public void openFile() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(
+                new FileChooser.ExtensionFilter("Text Files", "*.txt")
+        );
+
+        // choose file window
+        File file = fileChooser.showOpenDialog(new Stage());
+        if (file != null) {
+            String content = fileService.readFile(file);
+            textArea.setText(content);
+        }
+    }
 }
